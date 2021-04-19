bdd8a0e8b82fd90f547afeff486c3c46967b2928
navigaid <navigaid@gmail.com>
2021-04-19 14:26:11 +0800

    Remove .env file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 
+export BOOT1=/dns/82.157.37.77/tcp/30333/p2p/12D3KooWP44EhEGK9pp33nr19p3xKqUbnRWawgapeJ8Bbq3y4QmX
+export BOOT2=/dns/82.157.37.77/tcp/30334/p2p/12D3KooWM5QknCEcvJGJDKAE7uuBgbAcCXdFpiZHH9KLcw5oSUN5
+
 exec_nftmart(){
   exec nftmart \
     --telemetry-url="wss://telemetry.polkadot.io/submit/ 0" \
-    --name="${FUNCNAME[1]}" \
-    --log=runtime=debug \
+    --bootnodes=${BOOT1} \
+    --bootnodes=${BOOT2} \
     --execution=NativeElseWasm \
-    --rpc-cors=all \
-    --rpc-methods=Unsafe \
-    --prometheus-external \
     --unsafe-ws-external \
     --chain=staging_spec_raw.json \
     --wasm-execution=Interpreted \
-    --ws-max-connections=10000 \
     "$@"
 }
 
