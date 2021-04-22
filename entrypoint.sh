4b70be41b4a1910a9fe037d7bfe4ce04717b47d0
navigaid <navigaid@gmail.com>
2021-04-22 14:21:51 +0800

    Add validator node automatically

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-export BOOT1=/dns/82.157.37.77/tcp/30333/p2p/12D3KooWP44EhEGK9pp33nr19p3xKqUbnRWawgapeJ8Bbq3y4QmX
-export BOOT2=/dns/82.157.37.77/tcp/30334/p2p/12D3KooWM5QknCEcvJGJDKAE7uuBgbAcCXdFpiZHH9KLcw5oSUN5
+#export BOOT1=/dns/82.157.37.77/tcp/30333/p2p/12D3KooWP44EhEGK9pp33nr19p3xKqUbnRWawgapeJ8Bbq3y4QmX
+#export BOOT2=/dns/82.157.37.77/tcp/30334/p2p/12D3KooWM5QknCEcvJGJDKAE7uuBgbAcCXdFpiZHH9KLcw5oSUN5
+export BOOT1=/dns/43.129.73.167/tcp/30333/p2p/12D3KooWP44EhEGK9pp33nr19p3xKqUbnRWawgapeJ8Bbq3y4QmX
+export BOOT2=/dns/43.129.73.167/tcp/30334/p2p/12D3KooWM5QknCEcvJGJDKAE7uuBgbAcCXdFpiZHH9KLcw5oSUN5
 
 exec_nftmart(){
   exec nftmart \
@@ -13,6 +15,8 @@ exec_nftmart(){
     --unsafe-ws-external \
     --chain=staging_spec_raw.json \
     --wasm-execution=Interpreted \
+    --ws-port=9944 \
+    --port=30333 \
     "$@"
 }
 
