5dffc1c416b240abf514f60b325f8dbf91d2c7d0
navigaid <navigaid@gmail.com>
2021-07-06 14:43:12 +0800

    Init staging network

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env bash
 
-export BOOT1=/dns/82.157.37.77/tcp/30333/p2p/12D3KooWP44EhEGK9pp33nr19p3xKqUbnRWawgapeJ8Bbq3y4QmX
-export BOOT2=/dns/82.157.37.77/tcp/30334/p2p/12D3KooWM5QknCEcvJGJDKAE7uuBgbAcCXdFpiZHH9KLcw5oSUN5
+export BOOT=/ip4/81.70.132.13/tcp/30343/p2p/12D3KooWP44EhEGK9pp33nr19p3xKqUbnRWawgapeJ8Bbq3y4QmX
 
 exec_nftmart(){
   exec nftmart \
     --telemetry-url="wss://telemetry.polkadot.io/submit/ 0" \
-    --bootnodes=${BOOT1} \
-    --bootnodes=${BOOT2} \
+    --bootnodes ${BOOT} \
     --execution=NativeElseWasm \
     --rpc-methods=Unsafe \
     --unsafe-ws-external \
-    --chain=staging_spec_raw.json \
-    --wasm-execution=Interpreted \
+    --chain=staging \
     "$@"
 }
 
