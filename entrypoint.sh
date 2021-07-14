528387945c62e0ed6e9f645edee226108ee0ff93
navigaid <navigaid@gmail.com>
2021-07-14 15:40:03 +0800

    add more bootnodes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
-export BOOT=/ip4/81.70.132.13/tcp/30343/p2p/12D3KooWP44EhEGK9pp33nr19p3xKqUbnRWawgapeJ8Bbq3y4QmX
+export BOOT1=/dns/boot1.staging.nftmart.io/tcp/30333/p2p/12D3KooWEWdAjfxHM6re5yybcajpgMzgJEGTDWFw9ZrWTr1wicVQ
+export BOOT2=/dns/boot2.staging.nftmart.io/tcp/30333/p2p/12D3KooWP1BNyrk7Q1uMvhcmjAUwZjNRHjVwXu5jWhShvdFeQmPv
+export BOOT3=/dns/boot3.staging.nftmart.io/tcp/30333/p2p/12D3KooWJmqtRND4Q6rjFB6QaUnVBhQxfxkLm21Ax5Z1mppG6xPw
+
 
 exec_nftmart(){
   exec nftmart \
     --telemetry-url="wss://telemetry.polkadot.io/submit/ 0" \
-    --bootnodes ${BOOT} \
+    --bootnodes ${BOOT1} ${BOOT2} ${BOOT3} \
     --execution=NativeElseWasm \
     --rpc-methods=Unsafe \
     --unsafe-ws-external \
